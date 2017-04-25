57c89e82c6d8665eb96b6dc451eacb7ffd575ebf
Andrew Pelykh <andrew.pelykh@gmail.com>
2017-04-25 20:11:29 +0300

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  # Данний файл відповідає за всі роути
+
+  # Так ми визначаємо роут домашньої сторінки, вона буде привязяна до app/controllers/users_controller.rb і екшена index в ньому.
+  # Відповідна в'юха буде app/views/users/index.html.erb'
+  root "users#index"
 end
